feb10fdc6b7738a49b38c80c0da5190dd4b1ccce
dianaallam <allamdiana@gmail.com>
2014-11-23 13:02:28 +0100

    final

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ AdBuddizProject
 ===============
 
 String Analysis project
+
+
+In this repository, we present two implementation projects:
+
+1) a project using Jersey ("stringAnalysisProjectUsingJersey") which is deployed on Google App Engine and available at the following URL:
+http://stringanalysisapplication.appspot.com/stringAnalysisService/stringToAnalyze
+
+
+2) a project using CXF ("stringAnalysisProjectUsingCXF" and its client "stringAnalysisProjectUsingCXFTestClient") 
+which can be tested locally on an Apache Tomcat v8.0
+
+
+The CXF project has an advantage face to the Jersey project concerning the developped Code. For some compatibility reasons between CXF and GAE, 
+this project requires some more adaptations to be deployed at GAE.
+
+The advantages of the CXF project are:
+
+1) the Web service defines an interface which contains all the JAX-RS annotations. 
+The implementation class for this service is a Java class without any annotation. 
+The Advantages: We can provide different implementations of the Web service interface without the need for repeating the JAX-RS annotations on each implementation 
+class. Moreover, this interface could be used at a client implemented in Java to call the Web Service using the practices of Java
+development. The client makes a request by simplly calling an "analyzeString" method using an instance of "ResourceToAnalyze" 
+and gets in return an instance of "NonRepeatedCharacter".
+
+2) the resource classes ("ResourceToAnalyze" and "NonRepeatedCharacter") used as input and output data for the Web service do not contain any JAXB annotation.
+ That makes the Java code more elegant and allows the use of these classes in another context than for this Web Service.
+
+For more details about each project, please refer to the Readme.txt file in the src folder in each project.
+
+
--- stringAnalysisProjectUsingJersey/src/stringAnalysis/resources/ResourceToAnalyze.java
+++ stringAnalysisProjectUsingJersey/src/stringAnalysis/resources/ResourceToAnalyze.java
@@ -22,40 +22,80 @@ public class ResourceToAnalyze {
 	public Character getFirstNonRepeatedCharacter(){
 		
 		boolean continueAnalyze = true;
+		/*
+		 * The treatedCharacters set is used to save previously treated characters in 
+		 * the string
+		 */
+		
 		Set<Character> treatedCharacters = new HashSet<Character>();
+		
+		// charValueAti is a variable to save the current treated character
 		char charValueAti;
+		
+		/* charValueAtj is a variable used to stock the character resulted
+		after each iteration in the following loop */
 		char charValueAtj;
-		for(int i =0;  i < this.getStringToAnalyze().length(); i++){
+		
+		/* external loop to iterate on all characters */
+		
+		for(int i = 0;  i < this.getStringToAnalyze().length(); i++){
 			continueAnalyze=true;
 			charValueAti = this.getStringToAnalyze().charAt(i);
 			
+			/* verifying if charValueAti character is already treated */
 			Character c;
 			Iterator<Character> iterator =  treatedCharacters.iterator();
 			while(continueAnalyze &&  iterator.hasNext()){
 				c = iterator.next();
+				/* 
+				 * if charValueAti is already treated, stop the analysis for it and 
+				 * go to the next character
+				 */
 				if(c == charValueAti) continueAnalyze = false;
 			}
 			
+			/*
+			 * if charValueAti isn't an already treated character, verify if it is the last
+			 * character in the string
+			 */
 			if(i == this.getStringToAnalyze().length()-1 && continueAnalyze == true) 
+				
+				/*
+				 * if charValueAti is the last character, return it as the first non repeated 
+				 * character
+				 */
 				return charValueAti;
 			
+				/*
+				 * internal loop to iterate on all characters after charValueAti 
+				 */
 				else {
 					for(int j =i+1; continueAnalyze && j < this.getStringToAnalyze().length(); j++){
 						charValueAtj=this.getStringToAnalyze().charAt(j);
+						/*
+						 * if the character in iteration is equal to charValueAti,
+						 * stock this character in the list of treatedCharacter and stop 
+						 * the analysis
+						 */
 						if(charValueAti == charValueAtj){
 							continueAnalyze = false;
 							treatedCharacters.add(charValueAtj);
 						}
-						
+						/*
+						 * if the character in iteration is not equal to charValueAti and
+						 * it is the last character in the string, return 
+						 * charValueAti as the first non repeated character 
+						 */
 						else if(j == this.getStringToAnalyze().length()-1){
 							return charValueAti;
 						}
 					}
 				}
-
-	
 		}
 		
+		/*
+		 * if all characters are repeated, return null
+		 */
 		return null;
 	}
 
